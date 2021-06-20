73f41521062bfa5a830f3596a7e623146de6ec69
yaerse <yaerse@yahoo.com.tw>
2021-06-20 18:47:29 +0800

    [新增] 設定App的方向僅能是直式的

--- app/src/main/java/com/example/nasadatasetdemo/MainActivity.kt
+++ app/src/main/java/com/example/nasadatasetdemo/MainActivity.kt
@@ -1,21 +1,19 @@
 package com.example.nasadatasetdemo
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import com.example.nasadatasetdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setOrientationPortrait()
+    }
 
-        Toast.makeText(this, "${binding.textView.text}", Toast.LENGTH_SHORT).show()
+    private fun setOrientationPortrait() {
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT
     }
 }
