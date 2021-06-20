f0b2676aee5dc4b9387e8341798cd84b3d634bb4
yaerse <yaerse@yahoo.com.tw>
2021-06-20 22:15:51 +0800

    [新增] 完成HomeFragment的頁面呈現及按鈕點擊功能，其中按鈕被點擊後僅先用Toast呈現

--- app/src/main/java/com/example/nasadatasetdemo/view/home/HomeFragment.kt
+++ app/src/main/java/com/example/nasadatasetdemo/view/home/HomeFragment.kt
@@ -1,7 +1,10 @@
 package com.example.nasadatasetdemo.view.home
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.nasadatasetdemo.databinding.FragmentHomeBinding
 import com.example.nasadatasetdemo.view.base.BaseFragment
@@ -10,6 +13,18 @@ import com.example.nasadatasetdemo.view.base.BaseFragment
  * A simple [Fragment] subclass.
  */
 class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setListener()
+    }
+
+    private fun setListener() {
+        binding.request.setOnClickListener {
+            Toast.makeText(activity, "request", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun getFragmentBinding(
         inflater: LayoutInflater,
