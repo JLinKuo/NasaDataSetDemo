548de996fc60ba62a53e19ebfd48a2e2d7e89748
yaerse <yaerse@yahoo.com.tw>
2021-06-20 15:55:20 +0800

    [新增] 增加ViewBinding功能(MainActivity的部分是進行測試用)

--- app/src/main/java/com/example/nasadatasetdemo/MainActivity.kt
+++ app/src/main/java/com/example/nasadatasetdemo/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.nasadatasetdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.nasadatasetdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Toast.makeText(this, "${binding.textView.text}", Toast.LENGTH_SHORT).show()
     }
 }
